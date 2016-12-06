74f225595455cf8eed6b0211be9e8db7097c4bc6
liuyanhao <374659635@qq.com>
2016-12-06 20:45:39 +0800

    完善事件驱动

--- event_driven/app.js
+++ event_driven/app.js
@@ -1,15 +1,23 @@
-var http = require('http');
+var http = require('http'),
+	url = require('url');
 
-//监听服务器的request事件
+//侦听服务器的request事件
 http.createServer(function(req,res){
-	// res.writeHead('Allow0')
+	res.writeHead(200,{"Access-Control-Allow-Origin":"*"});
+	var callbackName = url.parse(req.url,true).query.callback;
+
 	var postData = '';
+    req.setEncoding("utf8");
+
+    //侦听请求的data事件
 	req.on('data',function(chunk){
 		postData += chunk;
 	})
+	//侦听请求的end事件
 	req.on('end',function(){
 		console.log(postData)
-		res.write(postData)
+		res.end(postData)
+		// res.end(callbackName+"("+ postData+")");
 	})
 }).listen(8001)
 console.log('listening port is',8001)
